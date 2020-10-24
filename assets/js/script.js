bd3bd439136f9ab52c9bbfbc0c6798e986ad729c
KHallam <kjhallam321@gmail.com>
2020-10-23 22:47:55 -0500

    updated pictures/image folder to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,10 @@ fetch(
 fetch(
   "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9"
 );
+
+//enter zip code (saves zip to local storage) append to a button to select previous location
+// displays weather (Location & Day)
+// displays 6 cards (closets)
+// color codes the trail ( "green" shortest distance, "yellow" < 7 mile hike, advanced > more than 7 mile hike (red))
+// get elevation and apply to the trails
+
