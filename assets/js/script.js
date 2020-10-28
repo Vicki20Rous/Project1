7f8418df547903f01d5f1d1df81a651c488b3b53
Victoria Rouser <vic86rous@gmail.com>
2020-10-27 20:55:46 -0500

    new stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 
 var todayDate;
 var cityName=[];
@@ -14,46 +16,12 @@ function getWeather(){
   .then(response =>{
   document.getElementById("image").src=response.current.weather_icon[0];
   document.getElementById("output").innerHTML="<h1>"+response.current.weather_description[0]+"</h1>Temperature:"+response.current.current.temperature+"<br>Feels like"+response.current.feelslike+"<br>UV index:"+response.current.uv_index+"<br>Humidity:"+response.current.humidity+"<br>Cloud cover:"+reponse.current.cloudcover;
+  console.log(getWeather)
   });  
   }
-    console.log (getWeather)
+   
 
-// async function currentWeather(city){
-//   const url= "https://api.weatherstack.com/current&units=m=609c6ca41a9803f4a3b46553fc8f5c5c"
-  
-//   return await $.ajax({
-//       url: url, 
-//       method: "GET"
-//       });
-//   }
-//display the trails
 
-//   function renderDayCard (locationName){ 
-//     $('#cards').empty();
-//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationName + "&units=imperial&appid=ed2ee741f61d9d60983426ca204e9ed6";
-//     $.ajax ({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response){
-//       console.log(response)
-// for(var i = 0; i < response.list.length; i++){
-//     if (response.list[i].dt_txt[12] === "5") {
-//     console.log(response.list[i].dt_txt);
-//      var card = $('<section>').addClass("card text-black bg-primary text-white").attr("style", "width: 7rem;");
-//      var cardBody = $('<div>').addClass("card-body");
-//     var cardDay = $('<h4>').addClass("card-title").text(moment.unix(response.list[i].dt).format("MM/DD/YYYY"));
-//     var cardTemp = $('<p>').addClass('card-text').text("Temp:" + response.list[i].main.temp + "F");
-//     var cardHumidity = $('<p>').addClass("card-text").text("Humidity: " + response.list[i].main.humidity + "%");
-//     var cardImg = $('<img>').attr("src","http://openweathermap.org/img/w/" + image + ".png");
-//      cardBody.append(cardDay, cardImg, cardTemp, cardHumidity);
-//      card.append(cardBody);
-//     $("#cards").append(card);
-//     }
-// }
-//  })
-// }
-
-//weather API key
 
 //hiking API Key
 function trailData() {
@@ -76,8 +44,8 @@ console.log(response)
 // get elevation and apply to the trails
 
 
-$("#button").on("click", async function(event){
-  var userinput= $("input").val()
-  //const currentWeatherResponse= await currentWeather(userinput)
-  renderDayCard(userinput)
+$("button").on("click", function(){
+  //var userinput= $("input").val()
+  //renderDayCard(userinput)
+})
 })
