280fff3c9ffedb45e74a00ca1da6019b03bc9f82
KHallam <kjhallam321@gmail.com>
2020-10-29 12:15:52 -0500

    displayed a card to show trails within the area

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         console.log('======================WEATHER RESPONSE======================\n', response , '=========================================\n')
         
         document.getElementById("image").src = response.current.weather_icons[0];
-        document.getElementById("output").innerHTML = "<h1>" + response.current.weather_descriptions[0] + "</h1>Temperature:" + response.current.temperature + "<br>Feels like:" + response.current.feelslike + "<br>UV index:" + response.current.uv_index + "<br>Humidity:" + response.current.humidity + "<br>Cloud cover:" + response.current.cloudcover;
+        document.getElementById("output").innerHTML = "<h3>" + response.location.name + "</h3>Temperature: " + response.current.temperature + "F" + "<br><hr>Feels like: " + response.current.feelslike + "<br><hr>UV index: " + response.current.uv_index + "<br><hr>Humidity: " + response.current.humidity + "<br><hr>Description: " + response.current.weather_descriptions;
         return {lat: response.location.lat , lon: response.location.lon}
       }).then(coords => {
         console.log('----------------coords-------------------------', coords)
@@ -36,7 +36,7 @@ $(document).ready(function () {
           $(trails).each(function(index, el) {
             console.log( `${index}----> `, el)
 
-             var dummyData = {
+             var labelData = {
                       ascent: 0,
                       conditionDate: "",
                       conditionDetails: null,
@@ -61,47 +61,18 @@ $(document).ready(function () {
                       type: "",
                       url: ""
             }
-          //  var cardElement = $('<div>').addClass('card')
-            // var card = $('<section>').addClass("card text-black bg-primary text-white").attr("style", "width: 7rem;");
-            var parent = $('#cardDisplay').attr("style", "width: 10rem");
-            var imageMedium  = $('<img>').attr('src', el.imgMedium, "style", "width: 200px;", "height: 200px;", "justify-content: center;")
-            //var cardDay = $('<h4>').addClass("card-title").text(moment.unix(conditionDate).format("MM/DD/YYYY"));
-            //var cardImg = $('<img>').addClass('card-img').text(":" + response.list[i].main.temp + "F");
-           //var cardHumidity = $('<p>').addClass("card-text").text("Humidity: " + response.list[i].main.humidity + "%");
-           
-         //  var cardImg = $('<img>').attr("src","http://openweathermap.org/img/w/" + image + ".png");
-           
-           //// cardBody.append(cardDay, cardImg, cardTemp, cardHumidity);
-           // card.append(cardBody);
-        //   $("#cards").append(card);
+          
+            var parent = $('#cardDisplay').attr("style", "width: 30rem");
+            var imageSm  = $('<img>').attr('src', el.imgSqSmall, "style", "width: 300px;", "height: 300px;", "margin: 5px");
+            var name = $('<h6>').text('' + el.name).attr("style", "margin-bottom: 2px");
+            var difficult = $('<p>').text('Difficult:' + el.difficulty);
+            var summary = $('<p>').text('' + el.summary).attr("style", "margin: 3px");
 
 
 
 
-            parent.append(imageMedium)
-
-
-
-
-
-           
-             // <div class="card">
-        
-              //   <div class="card-image">
-              //     <img src=`${trail.imgSqSmall}` width="200" height="200">
-              //     <span class="card-title">${trail.name}</span>
-              //   </div>
-        
-              //   <div class="card-content">
-              //     <p>Difficulty: ${trail.difficulty}</p>
-              //     <p>Rating: ${trail.stars}</p>
-              //   </div>
-        
-              //   <div class="card-action">
-              //     <a href="${trail.url}" target="_blank">Go to the trail site</a>
-              //   </div>
-        
-              // </div>
+            parent.append(imageSm, name, difficult, summary)
+            $("#cardDisplay").append(parent);
 
           })
           
@@ -115,11 +86,10 @@ $(document).ready(function () {
 
   //hiking API Key
 function trailData(lat,lon) {
-  // var lat = 40.0274;
-  // var lon = -105.2519;
+  
 
   $.ajax({
-    //url:"https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9",
+   
     url: "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lon + "&maxDistance=10&key=200952288-580b87b672e00ea3e6f7ec1e05ad0bb9",
     method: "GET"
   }).then(function (response) {
@@ -147,15 +117,4 @@ return trailTemplate;
 $("#trail-cards").html(trails.join(""))
   })
 }
-// trailData();
-
-//enter zip code (saves zip to local storage) append to a button to select previous location
-// displays weather (Location & Day)
-// displays 6 cards (closets)
-// color codes the trail ( "green" shortest distance, "yellow" < 7 mile hike, advanced > more than 7 mile hike (red))
-// get elevation and apply to the trails
-
-
-//$("#button").on("click", async function(event){
-  //var userinput= $("input").val()
-  //const currentWeatherResponse= await currentWeather(userinput)
+})
